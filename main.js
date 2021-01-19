d832f88325c7ff11fdc3dc1056b0a19a39dbb296
Diego Stiehl <seccomiro@gmail.com>
2021-01-19 16:59:24 -0300

    Create project with the base structure

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron');
+const windowStateKeeper = require('electron-window-state');
+
+let mainWindow;
+
+function createWindow() {
+  let state = windowStateKeeper({
+    defaultWidth: 500,
+    defaultHeight: 650,
+  });
+
+  mainWindow = new BrowserWindow({
+    x: state.x,
+    y: state.y,
+    width: state.width,
+    height: state.height,
+    minWidth: 350,
+    maxWidth: 650,
+    minHeight: 300,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadFile('renderer/main.html');
+
+  state.manage(mainWindow);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) createWindow();
+});
